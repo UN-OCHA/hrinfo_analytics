5898d5eeb4b7f110a222cf5c7aca64dca0cce4e6
Serban Teodorescu <teodorescu.serban@gmail.com>
2018-08-03 16:12:27 +0300

    attempt to use env variables for credentials when client_secret.json is not available

--- base.py
+++ base.py
@@ -5,11 +5,27 @@ import json
 import urllib.request
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
 
 # Set up Google Sheets connection
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+if os.path.isfile('client_secret.json'):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+else:
+    creds = {}
+    creds["type"] = "service_account"
+    creds["project_id"] = os.getenv('PROJECT_ID')
+    creds["private_key_id"] = os.getenv('PRIVATE_KEY_ID')
+    creds["private_key"] = os.getenv('PRIVATE_KEY')
+    creds["client_email"] = os.getenv('CLIENT_EMAIL')
+    creds["client_id"] = os.getenv('CLIENT_ID')
+    creds["auth_uri"] = "https://accounts.google.com/o/oauth2/auth"
+    creds["token_uri"] = "https://accounts.google.com/o/oauth2/token"
+    creds["auth_provider_x509_cert_url"] = "https://www.googleapis.com/oauth2/v1/certs"
+    creds["client_x509_cert_url"] = os.getenv('CLIENT_X509_CERT_URL')
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds, scope)
+
 gc = gspread.authorize(credentials)
 wks = gc.open("HR.info Analytics")
 
